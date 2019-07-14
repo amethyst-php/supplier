6a98c1873c33cd5c914556a732dedcecf6c2d423
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/SupplierManager.php
+++ src/Managers/SupplierManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Supplier newEntity()
+ * @method \Amethyst\Schemas\SupplierSchema getSchema()
+ * @method \Amethyst\Repositories\SupplierRepository getRepository()
+ * @method \Amethyst\Serializers\SupplierSerializer getSerializer()
+ * @method \Amethyst\Validators\SupplierValidator getValidator()
+ * @method \Amethyst\Authorizers\SupplierAuthorizer getAuthorizer()
+ */
 class SupplierManager extends Manager
 {
     use ConfigurableManager;
